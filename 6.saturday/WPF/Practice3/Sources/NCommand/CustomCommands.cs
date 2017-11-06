7910748567c54190ddd7d6f4d3735a7e8c2fa45e
Igor Kislyuk <igorkislyuk@icloud.com>
2017-11-06 23:29:44 +0300

    Add practice 3

---
+++ 6.saturday/WPF/Practice3/Sources/NCommand/CustomCommands.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NCommand
+{
+    class CustomCommands
+    {
+        private static RoutedUICommand launch_command;
+
+        public static RoutedUICommand Launch
+        {
+            get { return launch_command; }
+        }
+
+        static CustomCommands()
+        {
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.L, ModifierKeys.Control));
+            launch_command = new RoutedUICommand("Launch", "Launch", typeof(CustomCommands), myInputGestures);
+        }
+    }
+}
---
+++ 6.saturday/WPF/Practice3/Sources/NCommand/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NCommand
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var binding = new CommandBinding {Command = CustomCommands.Launch};
+            binding.Executed += Launch_Handler;
+            binding.CanExecute += new CanExecuteRoutedEventHandler(LaunchEnabled_Handler);
+
+            CommandBindings.Add(binding);
+        }
+
+        private void Launch_Handler(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Launch invoked");
+        }
+
+        private void LaunchEnabled_Handler(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = checkBox1.IsChecked ?? false;
+        }
+    }
+}
