c7bcc8f2f8259d55916535a366e27d25ba9832f6
Igor Kislyuk <igorkislyuk@icloud.com>
2017-11-16 19:36:43 +0300

    add lab 2 wpf

---
+++ 6.saturday/WPF/Lab2/Sources/Wpf_MVVM/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Wpf_MVVM.View;
+using Wpf_MVVM.ViewModel;
+
+namespace Wpf_MVVM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+
+            var mainWindow = new MainWindow
+            {
+                DataContext = new MainViewModel()
+
+            };
+            
+            mainWindow.Show();
+        }
+
+    }
+}
---
+++ 6.saturday/WPF/Lab2/Sources/Wpf_MVVM/Model/PeopleModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_MVVM.Model
+{
+    class PeopleModel: INotifyPropertyChanged
+    {
+        #region Implement INotyfyPropertyChanged members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Fields
+
+        private string _firstName;
+        private string _lastName;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Get or set first name.
+        /// </summary>
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (_firstName == value) return;
+
+                _firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+
+        /// <summary>
+        /// Get or set last name.
+        /// </summary>
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (_lastName == value) return;
+
+                _lastName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        #endregion
+    }
+}
