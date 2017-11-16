0e810d829c7633f307d799264d888afdcca37632
Igor Kislyuk <igorkislyuk@icloud.com>
2017-11-16 19:45:05 +0300

    Fix natasha's examples

---
+++ 6.saturday/different/Entity/CustomerManager/CustomerManager/CustomerViewer.cs
@@ -0,0 +1,77 @@
+﻿using CodeFirst;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomerManager
+{
+    public partial class CustomerViewer : Form
+
+    {
+        CodeFirst.SampleContext context;
+ 
+        byte[] Ph;
+        public CustomerViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CustomerViewer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Customer customer = new Customer
+                {
+                    Name = this.textBoxname.Text,
+                    Email = this.textBoxmail.Text,
+                    Age = Int32.Parse(this.textBoxage.Text),
+                    Photo = Ph
+                };
+                context.Database.CreateIfNotExists();
+                context.Customers.Add(customer);
+                context.SaveChanges();
+                textBoxname.Text = String.Empty;
+                textBoxmail.Text = String.Empty;
+                textBoxage.Text = String.Empty;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.ToString());
+            }
+        }
+
+        private void buttonFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog diag = new OpenFileDialog();
+            if (diag.ShowDialog() == DialogResult.OK)
+            {
+                Image bm = new Bitmap(diag.OpenFile());
+                ImageConverter converter = new ImageConverter();
+                Ph = (byte[])converter.ConvertTo(bm, typeof(byte[]));
+            }
+        }
+    }
+}
+
---
+++ 6.saturday/different/LINQsql/LINQsql/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Windows.Forms;
+
+
+namespace LINQsql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataContext db = new DataContext
+(fileOrServerOrConnection: @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;");
+
+
+            var results = from c in db.GetTable<Program.Customer>()
+                          where c.City == "London"
+                          select c;
+            foreach (var c in results)
+                listBox1.Items.Add(c.ToString());
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+   
+}
