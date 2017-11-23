35628b4ae66c33c0b5b7dd544e2d374f01e6bbc4
Igor Kislyuk <igorkislyuk@icloud.com>
2017-11-23 05:26:57 +0300

    Add asp.net lab 1

---
+++ 6.saturday/ASP.NET/Exercise1/Sources/MvcBasicWalkthrough.Tests/Controllers/MapsControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcBasicWalkthrough.Controllers;
+
+namespace MvcBasicWalkthrough.Tests.Controllers
+{
+    [TestClass]
+    public class MapsControllerTest
+    {
+        [TestMethod]
+        public void ViewMaps()
+        {
+            // Arrange
+            MapsController controller = new MapsController(); 
+            
+            // Act
+            ViewResult result = controller.ViewMaps() as ViewResult;
+            
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
