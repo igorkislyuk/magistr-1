f1f168cbe76887f13f2309c56e88bde3b7934c54
Igor Kislyuk <igorkislyuk@icloud.com>
2017-11-25 13:15:43 +0300

    ado add lab2.1

---
+++ 6.saturday/ADO.NET/Practice2/Sources/LINQ_SQL_1/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_SQL_1
+{
+    [Table(Name = "Customers")]
+    public class Customer
+    {
+        private string _CustomerID;
+
+        [Column(IsPrimaryKey = true, Storage = "_CustomerID")]
+        public string CustomerID
+        {
+            get { return this._CustomerID; }
+            set { this._CustomerID = value; }
+        }
+
+        private string _City;
+
+        [Column(Storage = "_City")]
+        public string City
+        {
+            get { return this._City; }
+            set { this._City = value; }
+        }
+
+        public override string ToString()
+        {
+            return CustomerID + "\t" + City;
+        }
+    }
+}
---
+++ 6.saturday/ADO.NET/Practice2/Sources/LINQ_SQL_1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Linq;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ_SQL_1
+{
+    public partial class Form1 : Form
+    {
+        DataContext db = new DataContext(@"Data Source=.\SQLExpress;Initial Catalog=NORTHWND;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var results = from c in db.GetTable<Customer>()
+                where c.City == "London"
+                select c;
+
+            foreach (var c in results)
+            {
+                listBox1.Items.Add(c.ToString());
+            }
+        }
+    }
+}
