6c0b6def922be8a50d4398475bb13b539f91a3dc
Igor Kislyuk <igorkislyuk@icloud.com>
2017-11-25 15:32:44 +0300

    Fix bug with crash

--- 6.saturday/ADO.NET/Practice3/Sources/CourseManager/CourseViewer.cs
+++ 6.saturday/ADO.NET/Practice3/Sources/CourseManager/CourseViewer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -29,8 +31,8 @@ namespace CourseManager
 
             try
             {
-                departmentList.DisplayMember = "Name";
-                departmentList.DataSource = ((ObjectQuery) departmentQuery).Execute(MergeOption.AppendOnly);
+                departmentList.ValueMember = "Name";
+                departmentList.DataSource = ((DbQuery<Department>) departmentQuery).ToList();
             }
             catch (Exception ex)
             {
@@ -42,15 +44,10 @@ namespace CourseManager
         {
             try
             {
-                Department department =
-                    (Department) this.departmentList.SelectedItem;
-                courseGridView.DataSource = department.Courses;
-                courseGridView.Columns["Department"].Visible = false;
-                courseGridView.Columns["StudentGrades"].Visible = false;
-                courseGridView.Columns["OnlineCourse"].Visible = false;
-                courseGridView.Columns["OnsiteCourse"].Visible = false;
-                courseGridView.Columns["People"].Visible = false;
-                courseGridView.Columns["DepartmentId"].Visible = false;
+                var department = (Department) departmentList.SelectedItem;
+
+                courseGridView.DataSource = department.Courses.ToList();
+
                 courseGridView.AllowUserToDeleteRows = false;
                 courseGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
             }
