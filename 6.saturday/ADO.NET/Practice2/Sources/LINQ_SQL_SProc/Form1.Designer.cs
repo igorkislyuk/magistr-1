af5ba1dd8745a37f4a7eea3d2e944dba5054c56f
Igor Kislyuk <igorkislyuk@icloud.com>
2017-11-25 13:50:42 +0300

    ado add lab 2.3. MDF Files included!!!

---
+++ 6.saturday/ADO.NET/Practice2/Sources/LINQ_SQL_SProc/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace LINQ_SQL_SProc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clientTextField = new System.Windows.Forms.TextBox();
+            this.codeTextField = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Order detail";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Client code";
+            // 
+            // clientTextField
+            // 
+            this.clientTextField.Location = new System.Drawing.Point(241, 204);
+            this.clientTextField.Name = "clientTextField";
+            this.clientTextField.Size = new System.Drawing.Size(194, 31);
+            this.clientTextField.TabIndex = 2;
+            // 
+            // codeTextField
+            // 
+            this.codeTextField.Location = new System.Drawing.Point(241, 270);
+            this.codeTextField.Name = "codeTextField";
+            this.codeTextField.Size = new System.Drawing.Size(194, 31);
+            this.codeTextField.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Order code";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(513, 368);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(231, 61);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Order history";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 591);
+            this.Controls.Add(this.codeTextField);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.clientTextField);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox clientTextField;
+        private System.Windows.Forms.TextBox codeTextField;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 6.saturday/ADO.NET/Practice2/Sources/LINQ_SQL_SProc/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ_SQL_SProc
+{
+    public partial class Form1 : Form
+    {
+        NORTHWND db =
+                new NORTHWND(
+                    @"Y:\Documents\study\magistr-2017-2018\6.saturday\ADO.NET\Practice2\Sources\LINQ_SQL_SProc\NORTHWND.MDF")
+            ;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            string param = codeTextField.Text;
+
+            var custquery = db.CustOrdersDetail(Convert.ToInt32(param));
+
+            string msg = "";
+            foreach (CustOrdersDetailResult custOrdersDetail in custquery)
+            {
+                msg = msg + custOrdersDetail.ProductName + "\n";
+            }
+            if (msg == "")
+                msg = "No results.";
+            MessageBox.Show(msg);
+
+            param = "";
+            codeTextField.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string param = clientTextField.Text;
+            var custquery = db.CustOrderHist(param);
+            string msg = "";
+            foreach (CustOrderHistResult custOrdHist in custquery)
+            {
+                msg = msg + custOrdHist.ProductName + "\n";
+            }
+            MessageBox.Show(msg);
+            param = "";
+            clientTextField.Text = "";
+        }
+    }
+}
