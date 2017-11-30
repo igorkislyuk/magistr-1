7795d11438b2d86a5c92ec9eca34f7a6c7ec2f4f
Igor Kislyuk <igorkislyuk@icloud.com>
2017-11-30 17:27:00 +0300

    Matlab lab 5 finish

--- 1.monday/modeling-and-optimization/5-optimization/Sources/task1.m
+++ 1.monday/modeling-and-optimization/5-optimization/Sources/task1.m
@@ -1,22 +1,60 @@
-%c = input('c=');
-%r = input('r=');
-%l = input('L=');
-%disp('Enter [a,b] and k:');
-%a = input('a=');
-%b = input('b=');
-%k = input('k=');
-%er = input('error=');
-[c,r,l,a,b,k,er] = textread('matlab1.txt','%f %f %f %f %f %f %f');
-w = 2*pi*a : 2*pi*k : 2*pi*b;
-g = r./(r.^2+(w*l-1./(w*c)).^2);
-pogr = (rand(1,length(g))-0.5)*2*er;
-new = g+g.*pogr;
-figure(1);
-plot(w,g,w,new);
-legend('theroretic calc', 'empiric calc');
-%s = fopen('_file','wb');
-%c = fwrite(s,a,'int');
-%j = fwrite(s,b,'int');
-%d = fwrite(s,k,'int');
-%u = fwrite(s,new,'double');
-%fclose(s);
+c1 = 2;
+c2 = 4;
+c3 = -1;
+c4 = -2;
+
+f = @(x1,x2)(c1*x1 +c2*x2 + c3*x1^2 +c4*x2^2);
+
+A = [-1 2; 2 1];
+b = [3;4];
+lb = zeros(2,1);
+d = 0.00001;
+
+x1 = 0;
+x2 = 0;
+
+k = 1;
+kmax = 100;
+
+x1trace = [x1];
+x2trace = [x2];
+
+i=2;
+while k<kmax;
+    gr1 = c1 + 2*c3*x1;
+    gr2 = c2 + 2*c4*x2;
+    Z = linprog([-gr1; -gr2], A, b, [], [], lb);
+    g = -((c1 + 2*c3*x1)*(Z(1) -x1) + ...
+        (c2 + 2*c4*x2)*(Z(2) -x2))/ ...
+        (2*c3*(Z(1) - x1)^2 + 2*c4*(Z(2) - x2)^2);
+    new_x1 = x1 + g*(Z(1) - x1);
+    new_x2 = x2 + g*(Z(2) - x2);
+    new_f = f(new_x1, new_x2);
+    old_f = f(x1,x2);
+    x1trace(i) = new_x1;
+    x2trace(i) = new_x2;
+    i = i+1;
+    x1 = new_x1;
+    x2 = new_x2;
+    if (new_f - old_f) / old_f <= d;
+        break;
+    end
+    k = k+1;
+end
+
+x = 0 : 0.1 : 2;
+y = 0 : 0.1 : 2;
+[X, Y] = meshgrid(x,y);
+plot([0 1], [1.5 2], 'r-', 'LineWidth', 3);
+hold on;
+plot([1 2], [2 0], 'r-', 'LineWidth', 3);
+Z = c1*X + c2* Y + c3*X.^2 + c4*Y.^2;
+[C, h] = contour(X, Y, Z);
+clabel(C, h);
+
+plot(x1trace, x2trace, '-');
+text(x1trace(1) - 0.1, x2trace(1) - 0.1, 'M0');
+text(x1 + 0.1, x2+ 0.1, ...
+    strvcat(['x1 = ' num2str(x1)],...
+            ['x2 = ' num2str(x2)],...
+            ['k = ' num2str(k)]));
--- 1.monday/modeling-and-optimization/5-optimization/Sources/task3.m
+++ 1.monday/modeling-and-optimization/5-optimization/Sources/task3.m
@@ -1,17 +1,13 @@
-c1 = 2;
-c2 = 4;
-c3 = -1;
-c4 = -2;
+c1 = -1;
+c2 = -1;
+a = 7;
+b = 7;
+R = 18;
 
-f = @(x1,x2)(c1*x1 +c2*x2 + c3*x1^2 +c4*x2^2);
+d = 0.01;
 
-A = [-1 2; 2 1];
-b = [3;4];
-lb = zeros(2,1);
-d = 0.00001;
-
-x1 = 0;
-x2 = 0;
+x1 = 6;
+x2 = 7;
 
 k = 1;
 kmax = 100;
@@ -19,42 +15,52 @@ kmax = 100;
 x1trace = [x1];
 x2trace = [x2];
 
+g = @(x1,x2)(R - (x1 - a)^2 - (x2 - b)^2);
+dfdx1 = @(x1,x2)(2*c1*x1);
+dfdx2 = @(x1,x2)(2*c2*x2);
+dgdx1 = @(x1,x2)(-2*x1+2*a);
+dgdx2 = @(x1,x2)(-2*x2 +2*b);
+
+gamma = 0.1;
+alpha = 1.9;
+
 i=2;
 while k<kmax;
-    gr1 = c1 + 2*c3*x1;
-    gr2 = c2 + 2*c4*x2;
-    Z = linprog([-gr1; -gr2], A, b, [], [], lb);
-    g = -((c1 + 2*c3*x1)*(Z(1) -x1) + ...
-        (c2 + 2*c4*x2)*(Z(2) -x2))/ ...
-        (2*c3*(Z(1) - x1)^2 + 2*c4*(Z(2) - x2)^2);
-    new_x1 = x1 + g*(Z(1) - x1);
-    new_x2 = x2 + g*(Z(2) - x2);
-    new_f = f(new_x1, new_x2);
-    old_f = f(x1,x2);
+    if g(x1,x2)>0;
+        alpha = 0;
+    else
+        alpha = max(0, alpha - gamma*g(x1,x2));
+    end
+    step_x1 = dfdx1(x1,x2) + alpha*dgdx1(x1,x2);
+    step_x2 = dfdx2(x1,x2) + alpha*dgdx2(x1,x2);
+        
+    new_x1 = max(0, x1+gamma*step_x1);
+    new_x2 = max(0, x2+gamma*step_x2);
+      
     x1trace(i) = new_x1;
     x2trace(i) = new_x2;
     i = i+1;
     x1 = new_x1;
     x2 = new_x2;
-    if (new_f - old_f) / old_f <= d;
+    if (dfdx1(x1,x2)/dgdx1(x1,x2)) - (dfdx2(x1,x2)/dgdx2(x1,x2)) <=d;
         break;
-    end
+    end;
     k = k+1;
 end
 
-x = 0 : 0.1 : 2;
-y = 0 : 0.1 : 2;
+x = 0 : 0.1 : 10;
+y = 0 : 0.1 : 10;
 [X, Y] = meshgrid(x,y);
-plot([0 1], [1.5 2], 'r-', 'LineWidth', 3);
+Zl = (X - a).^2 + (Y - b).^2;
+[C, h] = contour(X, Y, Zl, [R R], 'LineWidth',2);
 hold on;
-plot([1 2], [2 0], 'r-', 'LineWidth', 3);
-Z = c1*X + c2* Y + c3*X.^2 + c4*Y.^2;
+Z = c1*X.^2 + c2* Y.^2;
 [C, h] = contour(X, Y, Z);
 clabel(C, h);
+plot(x1trace, x2trace, '-+');
 
-plot(x1trace, x2trace, '-');
-text(x1trace(1) - 0.1, x2trace(1) - 0.1, 'M0');
-text(x1 + 0.1, x2+ 0.1, ...
+text(x1trace(1) + 0.1, x2trace(1), 'M0');
+text(x1 + 0.7, x2, ...
     strvcat(['x1 = ' num2str(x1)],...
             ['x2 = ' num2str(x2)],...
             ['k = ' num2str(k)]));
